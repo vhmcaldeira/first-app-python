53f537851a90be767638bd027ab1789155e51a36
˜vhmcaldeira˜ <caldeiravictor66@gmail.com>
2024-07-10 12:23:35 -0300

    alteravoes nas funcoes e excessao

--- app.py
+++ app.py
@@ -6,19 +6,33 @@ def opcoes():
     print ('3. ativar')
     print ('4. Sair\n')
 
+def finalizar_app():
+    os.system('clear')
+    print('Digite uma opcao valida \n')
+    main()
+
+def opcao_invalida():
+    print('opcao invalida\n')
+    main()
+
 
 def escolher_opcoes():
-    valor_bruto = int(input('Escolha um valor:'))
+    try:
+        valor_bruto = int(input('Escolha um valor:'))
 
-    if valor_bruto == 1:
-        print('voce escolheu cadastrar')
-    elif valor_bruto == 2:
-        print ('voce escolheu listar')
-    elif valor_bruto == 3:
-        print ('voce escolheu 3')
-    else:
-        os.system('clear')
-
+        if valor_bruto == 1:
+            print('voce escolheu cadastrar')
+        elif valor_bruto == 2:
+            print ('voce escolheu listar')
+        elif valor_bruto == 3:
+            print ('voce escolheu 3')
+        elif valor_bruto == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+            os.system('clear')
+    except:
+            opcao_invalida()
 def main():
     opcoes()
     escolher_opcoes()
